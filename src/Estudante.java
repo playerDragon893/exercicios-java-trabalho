7dae7da0696c48b10759853010df8c70c0a0358b
Juan Marley <2024301738@aluno.canoas.ifrs.edu.br>
2026-08-22 07:47:59 -0300

    Adiciona métodos de inserir notas e calcular média

--- src/Estudante.java
+++ src/Estudante.java
@@ -1,8 +1,53 @@
 package src;
 
+import java.util.Scanner;
+
 public class Estudante {
 
+    // Nome do estudante
     private String nome;
+
+    // Array com as 5 notas
     private double[] notas;
+
+    // Construtor da classe
+    public Estudante(String nome) {
+        this.nome = nome;
+        this.notas = new double[5];
+    }
+
+    // Metodo para inserir as 5 notas
+    public void insereNotas() {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.print("Digite a nota " + (i + 1) + ": ");
+            notas[i] = scanner.nextDouble();
+        }
+    }
+
+    // Metodo para calcular a média
+    public double calculaMedia() {
+        double soma = 0;
+
+        for (int i = 0; i < notas.length; i++) {
+            soma += notas[i];
+        }
+
+        return soma / notas.length;
+    }
+
+    // Metodo para encontrar a menor nota
+    public double menorNota() {
+        double menor = notas[0];
+
+        for (int i = 1; i < notas.length; i++) {
+            if (notas[i] < menor) {
+                menor = notas[i];
+            }
+        }
+
+        return menor;
+    }
 
 }
